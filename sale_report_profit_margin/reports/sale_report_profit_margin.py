a45c3987141ec47fc95235d04973af317072b36c
Azucena Luque <azucena@trey.es>
2015-01-14 11:36:12 +0100

    [IMP] Modificación para llamar al super de las partes que forman las consultas e insertar mis cambios después.

--- sale_report_profit_margin/reports/sale_report_profit_margin.py
+++ sale_report_profit_margin/reports/sale_report_profit_margin.py
@@ -33,173 +33,22 @@ class SaleReport(models.Model):
         readonly=True)
 
     def _select(self):
-        select_str = """
-             SELECT min(l.id) as id,
-                    l.product_id as product_id,
-                    t.uom_id as product_uom,
-                    sum(l.product_uom_qty / u.factor * u2.factor) as product_uom_qty,
-                    sum(l.product_uom_qty * l.price_unit * (100.0-l.discount) / 100.0) as price_total,
-                    count(*) as nbr,
-                    s.date_order as date,
-                    s.date_confirm as date_confirm,
-                    s.partner_id as partner_id,
-                    s.user_id as user_id,
-                    s.company_id as company_id,
-                    extract(epoch from avg(date_trunc('day',s.date_confirm)-date_trunc('day',s.create_date)))/(24*60*60)::decimal(16,2) as delay,
-                    s.state,
-                    t.categ_id as categ_id,
-                    s.pricelist_id as pricelist_id,
-                    s.project_id as analytic_account_id,
-                    s.section_id as section_id,
-
-                    l.margin as margin,
-                    rp.state_id as state_id
-        """
-        return select_str
+        select_str = super(SaleReport, self)._select()
+        return '%s %s' % (
+            select_str,
+            ',l.margin as margin'
+            ',rp.state_id as state_id')
 
     def _from(self):
-        from_str = """
-                sale_order_line l
-                      join sale_order s on (l.order_id=s.id)
-                        left join product_product p on (l.product_id=p.id)
-                            left join product_template t on (p.product_tmpl_id=t.id)
-
-                    left join res_partner rp on (rp.id=s.partner_id)
-
-                    left join product_uom u on (u.id=l.product_uom)
-                    left join product_uom u2 on (u2.id=t.uom_id)
-        """
-        return from_str
+        from_str = super(SaleReport, self)._from()
+        return '%s %s' % (
+            from_str,
+            'left join res_partner rp on (rp.id=s.partner_id)')
 
     def _group_by(self):
-        group_by_str = """
-            GROUP BY l.product_id,
-                    l.order_id,
-                    t.uom_id,
-                    t.categ_id,
-                    s.date_order,
-                    s.date_confirm,
-                    s.partner_id,
-                    s.user_id,
-                    s.company_id,
-                    s.state,
-                    s.pricelist_id,
-                    s.project_id,
-                    s.section_id,
-
-                    l.margin,
-                    rp.state_id
-        """
+        group_by_str = super(SaleReport, self)._group_by()
+        return '%s %s' % (
+            group_by_str,
+            ',l.margin'
+            ',rp.state_id')
         return group_by_str
-
-
-
-# from openerp import tools
-# from openerp.osv import fields, osv
-
-# class sale_report_profit_margin(osv.osv):
-#     _name = "sale.report.profit.margin"
-#     _description = "Sales orders profit margin statistics"
-#     _auto = False
-#     _rec_name = 'date'
-
-#     _columns = {
-#         'date': fields.datetime('Date Order', readonly=True),  # TDE FIXME master: rename into date_order
-#         'date_confirm': fields.date('Date Confirm', readonly=True),
-#         'product_id': fields.many2one('product.product', 'Product', readonly=True),
-#         'product_uom': fields.many2one('product.uom', 'Unit of Measure', readonly=True),
-#         'product_uom_qty': fields.float('# of Qty', readonly=True),
-
-#         'partner_id': fields.many2one('res.partner', 'Partner', readonly=True),
-#         'company_id': fields.many2one('res.company', 'Company', readonly=True),
-#         'user_id': fields.many2one('res.users', 'Salesperson', readonly=True),
-#         'price_total': fields.float('Total Price', readonly=True),
-#         'delay': fields.float('Commitment Delay', digits=(16,2), readonly=True),
-#         'categ_id': fields.many2one('product.category','Category of Product', readonly=True),
-#         'nbr': fields.integer('# of Lines', readonly=True),  # TDE FIXME master: rename into nbr_lines
-#         'state': fields.selection([
-#             ('draft', 'Quotation'),
-#             ('waiting_date', 'Waiting Schedule'),
-#             ('manual', 'Manual In Progress'),
-#             ('progress', 'In Progress'),
-#             ('invoice_except', 'Invoice Exception'),
-#             ('done', 'Done'),
-#             ('cancel', 'Cancelled')
-#             ], 'Order Status', readonly=True),
-#         'pricelist_id': fields.many2one('product.pricelist', 'Pricelist', readonly=True),
-#         'analytic_account_id': fields.many2one('account.analytic.account', 'Analytic Account', readonly=True),
-#         'section_id': fields.many2one('crm.case.section', 'Sales Team'),
-
-#         'margin': fields.float('Margin', readonly=True),
-#         'state_id': fields.many2one('res.country.state', 'State', readonly=True),
-#     }
-#     _order = 'date desc'
-
-#     def _select(self):
-#         select_str = """
-#              SELECT min(l.id) as id,
-#                     l.product_id as product_id,
-#                     t.uom_id as product_uom,
-#                     sum(l.product_uom_qty / u.factor * u2.factor) as product_uom_qty,
-#                     sum(l.product_uom_qty * l.price_unit * (100.0-l.discount) / 100.0) as price_total,
-#                     count(*) as nbr,
-#                     s.date_order as date,
-#                     s.date_confirm as date_confirm,
-#                     s.partner_id as partner_id,
-#                     s.user_id as user_id,
-#                     s.company_id as company_id,
-#                     extract(epoch from avg(date_trunc('day',s.date_confirm)-date_trunc('day',s.create_date)))/(24*60*60)::decimal(16,2) as delay,
-#                     s.state,
-#                     t.categ_id as categ_id,
-#                     s.pricelist_id as pricelist_id,
-#                     s.project_id as analytic_account_id,
-#                     s.section_id as section_id,
-
-#                     l.margin as margin,
-#                     rp.state_id as state_id
-#         """
-#         return select_str
-
-#     def _from(self):
-#         from_str = """
-#                 sale_order_line l
-#                       join sale_order s on (l.order_id=s.id)
-#                         left join product_product p on (l.product_id=p.id)
-#                             left join product_template t on (p.product_tmpl_id=t.id)
-
-#                     left join res_partner rp on (rp.id=s.partner_id)
-
-#                     left join product_uom u on (u.id=l.product_uom)
-#                     left join product_uom u2 on (u2.id=t.uom_id)
-#         """
-#         return from_str
-
-#     def _group_by(self):
-#         group_by_str = """
-#             GROUP BY l.product_id,
-#                     l.order_id,
-#                     t.uom_id,
-#                     t.categ_id,
-#                     s.date_order,
-#                     s.date_confirm,
-#                     s.partner_id,
-#                     s.user_id,
-#                     s.company_id,
-#                     s.state,
-#                     s.pricelist_id,
-#                     s.project_id,
-#                     s.section_id,
-
-#                     l.margin,
-#                     rp.state_id
-#         """
-#         return group_by_str
-
-#     def init(self, cr):
-#         # self._table = sale_report_profit_margin
-#         tools.drop_view_if_exists(cr, self._table)
-#         cr.execute("""CREATE or REPLACE VIEW %s as (
-#             %s
-#             FROM ( %s )
-#             %s
-#             )""" % (self._table, self._select(), self._from(), self._group_by()))
